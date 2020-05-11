c256229dcaaada77b546b1c71b1550631797b65e
simon-10 <65138110+simon-10@users.noreply.github.com>
2020-05-11 14:47:28 -0700

    updte someyhing

--- TMCheckingSystem/src/main/java/cs544/eaproject/Application.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/Application.java
@@ -8,7 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println();
+		System.out.println("Simon");
+		
 	}
 
 }
