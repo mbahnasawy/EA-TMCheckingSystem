d9e31af71455c2e222b3953ad2f0fbfafd730298
Erhmee <erhembayar.ch@gmail.com>
2020-05-11 18:39:30 -0500

    createAppointment

--- TMCheckingSystem/src/main/java/cs544/eaproject/controller/AppointmentController.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/controller/AppointmentController.java
@@ -2,7 +2,9 @@ package cs544.eaproject.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,27 +17,27 @@ import cs544.eaproject.util.ResponseStatus;
 @RestController
 public class AppointmentController {
 
-	@AutoWired
+	@Autowired
 	AppointmentService appointmentService;
 
-	@RequestMaping(value = "appointment/create", method=RequestMethod.POST, headers = "Accept=application/json", produces = {"application/json" })
-	public Appointment createAppointment(@RequestBody Appointment appointmentDTO) {
-		
+	@RequestMapping(value = "appointment/create", method = RequestMethod.POST, headers = "Accept=application/json", produces = {
+			"application/json" })
+	public Response createAppointment(@RequestBody Appointment appointmentDTO) {
+
 		Response response = new Response();
-		
+
 		try {
 			Appointment appointment = appointmentService.createAppointment(appointmentDTO);
 			response.setResult(appointment);
 			response.setMessage(ResponseStatus.SUCCESS_MESSAGE);
 			response.setStatus(ResponseStatus.SUCCESS_CODE);
-			
-		}catch(Exception ex) {
+
+		} catch (Exception ex) {
 			response.setMessage(ex.getMessage());
 			response.setStatus(ResponseStatus.ERROR_CODE);
 		}
-		
-		return appointment;
+
+		return response;
 	}
-	
-	
+
 }
