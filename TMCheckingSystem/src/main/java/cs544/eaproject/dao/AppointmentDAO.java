c8df1af768d0b819649693afff5a4bc5562cd694
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-05-12 10:39:59 -0500

    AppointmentDAO

--- TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAO.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAO.java
@@ -1,43 +1,13 @@
 
-  package cs544.eaproject.dao;
+ package cs544.eaproject.dao;
 
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import org.hibernate.boot.model.relational.Database;
 
 import cs544.eaproject.domain.Appointment;
 
-public class AppointmentDAO{
-	private Database db;
-	
-	public AppointmentDAO(Database db) {
-		this.db=db;
-		
-	}
-	public List<Appointment> findall(){
 
-	List<Appointment> appointment=new ArrayList<Appointment>();
-	ResultSet rs;
-	try {
-		rs = db.runSql("select * from appointment");
-		while(rs.next()) {
-			Integer appointmnetId=Integer.parseInt(rs.getString("id"));
-			String dataTime=rs.getString("dataTime");
-			String location=rs.getString("location");
-			String provider=rs.getString("provider");
-			
-			appointment.add(new Appointment(id, dateTime, location, provider));
-		}
-		return appointment;
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		return null;
+public interface AppointmentDAO extends CommonDAO<Appointment> {
+	List<Appointment> getAll();
 }
-}
-}
+
