af23dabad32b57979ac0b4759245f17e6fea125f
Erhmee <erhembayar.ch@gmail.com>
2020-05-12 13:05:26 -0500

    dev override

--- TMCheckingSystem/src/main/java/cs544/eaproject/Application.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/Application.java
@@ -1,15 +1,25 @@
 package cs544.eaproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import cs544.eaproject.config.AppConfig;
+import cs544.eaproject.service.ReservationService;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		
-		
+		SpringApplication.run(Application.class);
+
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
 }
