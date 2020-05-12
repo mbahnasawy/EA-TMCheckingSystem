c146ec9fda89fdf87b0b90cbdff51edfbe717179
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-05-11 23:03:21 -0500

    Snippet

--- TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAOImpl.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAOImpl.java
@@ -1,5 +1,18 @@
 package cs544.eaproject.dao;
 
-public class AppointmentDAOImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.Query;
+
+import cs544.eaproject.domain.Appointment;
+
+public class AppointmentDAOImpl extends BaseDAOImpl<Appointment> implements AppointmentDAO {
+	
+	public List<Appointment> getAll() {
+		Query query = sessionFactory.getCurrentSession().createQuery("SELECT a from Appointment a");
+		return new ArrayList<Appointment>(query.list());
+	}
+	
 
 }
