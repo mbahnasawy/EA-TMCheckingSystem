d2cc35a527d1b2bbc52f2b08c3d59ef2fa8e97bc
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-05-12 13:32:54 -0500

    CommonDAOImpl

---
+++ TMCheckingSystem/src/main/java/cs544/eaproject/dao/CommonDAOImpl.java
@@ -0,0 +1,48 @@
+package cs544.eaproject.dao;
+
+	import java.lang.reflect.ParameterizedType;
+	import java.lang.reflect.Type;
+
+	import org.hibernate.Session;
+	import org.hibernate.SessionFactory;
+	import org.springframework.beans.factory.annotation.Autowired;
+
+	public abstract class CommonDAOImpl<T> implements CommonDAO<T> {
+
+		@Autowired
+		protected SessionFactory sessionFactory;
+
+		private Class<T> type;
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public CommonDAOImpl() {
+			Type t = getClass().getGenericSuperclass();
+			ParameterizedType pt = (ParameterizedType) t;
+			type = (Class) pt.getActualTypeArguments()[0];
+		}
+		
+		public T add(T t) {
+			sessionFactory.getCurrentSession().save(t);
+			return t;
+		}
+
+		public T update(T t) {
+			sessionFactory.getCurrentSession().saveOrUpdate(t);
+			return t;
+		}
+
+		public boolean delete(int id) {
+			Session session = sessionFactory.getCurrentSession();
+			session.delete((T) session.load(type, id));
+			return true;
+		}
+
+		public T get(int id) {
+			return (T) sessionFactory.getCurrentSession().get(type, id);
+		}
+		
+		
+	}
+
+	
+
