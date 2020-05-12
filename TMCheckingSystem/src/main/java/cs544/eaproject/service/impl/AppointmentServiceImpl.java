f90d9b57db2b3f18f5708968db4f29c44ca8ab27
Erhmee <erhembayar.ch@gmail.com>
2020-05-11 22:32:27 -0500

    AppointServiceImpl

--- TMCheckingSystem/src/main/java/cs544/eaproject/service/impl/AppointmentServiceImpl.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/service/impl/AppointmentServiceImpl.java
@@ -1,14 +1,15 @@
 package cs544.eaproject.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import cs544.eaproject.dao.AppointmentDAO;
 import cs544.eaproject.domain.Appointment;
 import cs544.eaproject.domain.User;
+import cs544.eaproject.service.AppointmentService;
 
-public class AppointmentServiceImpl {
+public class AppointmentServiceImpl implements AppointmentService {
 
 	@Autowired
 	private AppointmentDAO appointmentDao;
@@ -18,5 +19,23 @@ public class AppointmentServiceImpl {
 //		appointmentDao.create(appointment);
 
 		return appointment;
+	}
+
+	@Override
+	public void cancelAppointment(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Appointment viewAppointment(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Appointment> getAllAppointmentsByChecker(int tmCheckerId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
