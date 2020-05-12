49a60a33ff94adb8971b9bb03c5141da08579021
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-05-12 10:51:15 -0500

    AppointmentDAOImpl

--- TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAOImpl.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/dao/AppointmentDAOImpl.java
@@ -1,29 +1,24 @@
 
 package cs544.eaproject.dao;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import javax.management.Query;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import cs544.eaproject.domain.Appointment;
-import cs544.eaproject.domain.Reservation;
+
+
 
 @Transactional
 @Repository
 public class AppointmentDAOImpl extends CommonDAOImpl<Appointment> implements AppointmentDAO {
 
-	@SuppressWarnings("unchecked")
-	public List<Appointment> getAll() {
-		Query query = sessionFactory.getCurrentSession().createQuery("SELECT a from Appointment a");
-		return new ArrayList<Appointment>(query.list());
+	public List<Appointment> getAll()
+	{
+		// TODO Auto-generated method stub
+				return null;
 		
 	}
-	
 }
 
