61699a4a757e529d2defc40b3f6fe5e529b37b4b
Kareem Salah <37020590+Kymo97@users.noreply.github.com>
2020-05-12 03:52:54 +0200

    Project entities

---
+++ TMCheckingSystem/src/main/java/cs544/eaproject/domain/Role.java
@@ -0,0 +1,28 @@
+package cs544.eaproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
