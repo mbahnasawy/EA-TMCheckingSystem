5a79f60a564b0877be776bce355ab022283822e1
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-05-12 16:21:19 -0500

    Create Reservation

--- TMCheckingSystem/src/main/java/cs544/eaproject/controller/ReservationController.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/controller/ReservationController.java
@@ -1,17 +1,24 @@
 package cs544.eaproject.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cs544.eaproject.domain.Appointment;
 import cs544.eaproject.service.ReservationService;
 import cs544.eaproject.service.dto.ReservationDto;
 
@@ -43,4 +50,13 @@ public class ReservationController {
 	public void cancelReservation(@PathVariable long id)throws Exception{
 		reservationService.cancelReservation(id);
 	}
+	
+	@RequestMapping(value = "/create", params="appointmentId", method = RequestMethod.POST, headers = "Accept=application/json", produces = {
+	"application/json" })
+	public ReservationDto createReservation(@RequestParam String appointmentId)throws Exception{
+
+		return reservationService.createReservation(Long.parseLong(appointmentId));
+	}
+	
+	
 }
