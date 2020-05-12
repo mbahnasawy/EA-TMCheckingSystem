8274b16eb7facd3eabea73b156046d4b79e241d0
Erhmee <erhembayar.ch@gmail.com>
2020-05-11 23:07:14 -0500

    override

--- TMCheckingSystem/src/main/java/cs544/eaproject/domain/Role.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/domain/Role.java
@@ -2,13 +2,14 @@ package cs544.eaproject.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 @Entity
 public class Role {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String name;
