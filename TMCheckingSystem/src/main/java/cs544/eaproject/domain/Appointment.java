be0186815adbc8df5b0f104ef7679a42d0b17442
Erhmee <erhembayar.ch@gmail.com>
2020-05-11 22:48:11 -0500

    appointment, reservation restore

---
+++ TMCheckingSystem/src/main/java/cs544/eaproject/domain/Appointment.java
@@ -0,0 +1,96 @@
+package cs544.eaproject.domain;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+
+@Entity
+public class Appointment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateTime;
+
+	@Column(nullable = false)
+	private String location;
+
+	@OneToMany(mappedBy = "appointment",cascade = CascadeType.ALL)
+	private Set<Reservation> reservations;
+
+	@ManyToOne
+	private User provider;
+
+	public Appointment() {
+	}
+
+	public Appointment(Date dateTime, String location, User provider) {
+		this.dateTime = dateTime;
+		this.location = location;
+		this.provider = provider;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public User getProvider() {
+		return provider;
+	}
+
+	public void setProvider(User provider) {
+		this.provider = provider;
+	}
+
+	public Set<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(Set<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+
+	public void addReservation(Reservation reservation) {
+		// add this
+		reservation.setAppointment(this);
+		reservations.add(reservation);
+
+	}
+}
