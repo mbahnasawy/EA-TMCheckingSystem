9b18dbc72579c6e6e2bbf183fa4af7be97849768
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-05-14 10:14:42 -0500

    Testing

--- TMCheckingSystem/src/test/java/cs544/eaproject/repository/AppointmentRepositoryTest.java
+++ TMCheckingSystem/src/test/java/cs544/eaproject/repository/AppointmentRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import cs544.eaproject.domain.Appointment;
@@ -17,6 +18,7 @@ import cs544.eaproject.domain.User;
  
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@SpringBootTest
 public class AppointmentRepositoryTest {
 
 	@Autowired
@@ -28,8 +30,8 @@ public class AppointmentRepositoryTest {
     @Test
     public void testRepository() 
     {
-		User provider = new User("Ahmed", "Yassen", "Male", "x@y.com", roleRepository.findById(3L).get(), "P@ssw0rd",
-				"xyz");
+		User provider = new User("Ahmed", "Yassen", "Male", "x@y.com", 
+				roleRepository.findById(3L).get(), "P@ssw0rd","xyz");
 
 		GregorianCalendar calendar = new GregorianCalendar(2021, Calendar.FEBRUARY, 20, 18, 9, 22);
 
