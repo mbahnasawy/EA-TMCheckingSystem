93b065da65ea7e7aeca319347f03280b107b9ad4
Kareem Salah <37020590+Kymo97@users.noreply.github.com>
2020-05-14 11:12:34 +0200

    Update userDto.java

--- TMCheckingSystem/src/main/java/cs544/eaproject/service/dto/userDto.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/service/dto/userDto.java
@@ -1,10 +1,34 @@
 package cs544.eaproject.service.dto;
 
-public class userDto {
-	
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class UserDto {
+
+	private Long id;
+	private String firstName;
 	private String lastName;
-	
+	private String gender;
 	private String email;
+	private String userName;
+	private String password;
+	private int roleId;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 	public String getLastName() {
 		return lastName;
@@ -14,6 +38,14 @@ public class userDto {
 		this.lastName = lastName;
 	}
 
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public String getEmail() {
 		return email;
 	}
@@ -21,4 +53,31 @@ public class userDto {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+
 }
