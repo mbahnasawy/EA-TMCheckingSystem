fb9109fcf9a1ef5c48742bfe93e3ebf7940b93ba
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-05-14 03:10:09 -0500

    User Service create

--- TMCheckingSystem/src/main/java/cs544/eaproject/controller/UserController.java
+++ TMCheckingSystem/src/main/java/cs544/eaproject/controller/UserController.java
@@ -3,18 +3,24 @@ package cs544.eaproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cs544.eaproject.integration.Jwt;
 import cs544.eaproject.service.UserService;
+import cs544.eaproject.service.dto.AppointmentDto;
 import cs544.eaproject.service.dto.AuthRequest;
 import cs544.eaproject.service.dto.AuthResponse;
+import cs544.eaproject.service.dto.ReservationDto;
+import cs544.eaproject.service.dto.UserDto;
 
 @RestController
 @RequestMapping("/user")
@@ -38,6 +44,14 @@ public class UserController {
 		}
 		final UserDetails user = userService.loadUserByUsername(authRequest.getUsername());
 		String token = jwt.generateToken(user.getUsername());
-		return ResponseEntity.ok(new AuthResponse(token, "Login success",HttpStatus.OK.value()));
+		return ResponseEntity.ok(new AuthResponse(token, "Login success", HttpStatus.OK.value()));
+	}
+
+	@RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json", produces = {
+			"application/json" })
+	@Secured({ "ROLE_ADMIN" })
+	public UserDto createUser(@RequestBody UserDto userDto) throws Exception {
+
+		return userService.createUser(userDto);
 	}
 }
